48a28f34f7c4310a87f9fceee652e3951e837273
Simon Lexau <lexau@live.no>
2019-04-01 18:59:05 +0200

    new timer, fixed queue functions

--- rammeverk/source/queue.h
+++ rammeverk/source/queue.h
@@ -1,13 +1,17 @@
-#ifndef
-enum State = [INIT, IDLE, MOVE, WAIT, EM_STOP];
-#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include "elev.h"
+#include "elevator.h"
+typedef enum State { INIT, IDLE, MOVE, WAIT, EM_STOP } State;
+
+int a = IDLE;
 
 // queue is a matrix on the form:
 //		| 1	| 2	| 3	| 4	|
 //	UP	| x	| x	| x	| x	|
 // Down	| x	| x	| x	| x	|
 //
-int[N_FLOORS][2] queue;
+int queue[2][N_FLOORS];
 
 int checkForOrders();
 
--- rammeverk/source/timer.c
+++ rammeverk/source/timer.c
@@ -1,11 +1,9 @@
 #include "timer.h"
-#include <time.h>
 
-void timer(){
-    int sec = 0, trigger = 3;
-    clock_t before clock();
-    do{
-        clock_t difference = clock() - before;
-        sec = difference / CLOCKS_PER_SEC;
-    } while ( sec < trigger);
+time_t getTimer() {
+	return (timer - time(NULL));
+}
+
+void startTimer() {
+	timer = time(NULL);
 }
